e478dde3d095a7683ad87d3aea6b9b3bb8688c91
moonrivervoyager <carolinejoannasmith@gmail.com>
2023-01-29 23:05:55 -0800

    NavBarProgress
    Started Nav Bar

---
+++ PORTFOLIO/index.js
@@ -0,0 +1,6 @@
+const toggleeButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
